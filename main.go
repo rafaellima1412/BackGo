735db0c98281b2b72f065cd66ed50ae9a8fa5044
rafael.lima <rafael.lima@archoffice.tech>
2023-06-20 15:11:45 -0400

    criando rotas do crud

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"backgo/config"
+	"backgo/router"
+)
+
+var (
+	logger *config.Logger
+)
+
+func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		// fmt.Println(err)
+		logger.Errf("config initialization error: %v", err)
+		return
+	}
+	router.Initialize()
+
+}
