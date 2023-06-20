90dfe183d9f31aa237f8998a70d2ae8dc9503509
rafael.lima <rafael.lima@archoffice.tech>
2023-06-20 14:44:20 -0400

    config mysql e logger

--- config/mysql.go
+++ config/mysql.go
@@ -1,33 +1,33 @@
 package config
 
 import (
+	"backgo/schemas"
 	"os"
 
-	//"example.com/estudoGo/schemas"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitializeMysql() (*gorm.DB, error) {
-	//logger := GetLogger("mysql")
+	logger := GetLogger("mysql")
 
 	_, err := os.Stat("mysql")
 
 	if os.IsNotExist(err) {
-		//logger.Info("database not found, creating....")
+		logger.Info("database not found, creating....")
 	}
 
 	dsn := "root:root@tcp(127.0.0.1:3306)/mysqldb?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		//	logger.Err("Mysql initialization error")
+		logger.Err("Mysql initialization error")
 		return nil, err
 	}
 
 	err = db.AutoMigrate(&schemas.Opening{})
 	if err != nil {
-		//	logger.Err("Mysql automigration error")
+		logger.Err("Mysql automigration error")
 		return nil, err
 	}
 
