fcc051c9b29f628cc0119d5d929ef7352122d4d3
rafael.lima <rafael.lima@archoffice.tech>
2023-06-21 10:29:16 -0400

    completando o crud

--- router/routes.go
+++ router/routes.go
@@ -1,32 +1,38 @@
 package router
 
 import (
-	"backgo/handler"
-
 	"backgo/docs"
+	"backgo/handler"
 
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 func InitializeRoutes(router *gin.Engine) {
 
 	basePath := "/api/v1"
 	handler.InitializeHandler()
-	// programmatically set swagger info
-	docs.SwaggerInfo.Title = "Swagger Example API"
-	docs.SwaggerInfo.Description = "This is a sample server BackGo server."
-	docs.SwaggerInfo.Version = "1.0"
+
+	docs.SwaggerInfo.Title = "Swagger BackGo"
+	docs.SwaggerInfo.Description = "This is a  BackGo server."
+	docs.SwaggerInfo.Version = "2.0"
 	docs.SwaggerInfo.Host = "BackGo.swagger.io"
+	docs.SwaggerInfo.BasePath = basePath
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 	v1 := router.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
-		// v1.POST("/opening", handler.CreateOpeningHandler)
-		// v1.DELETE("/opening", handler.DeleteOpeningHandler)
-		// v1.PUT("/opening", handler.UpdateOpeningHandler)
-		// v1.GET("/openings", handler.ListOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningHandler)
 	}
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
